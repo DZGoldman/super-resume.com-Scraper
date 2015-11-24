bdd6c2919858915c87d3527c5657e9680b54cfb9
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-11-24 13:14:16 -0500

    scrappin'

--- server.js
+++ server.js
@@ -23,37 +23,66 @@ var casper = require('casper').create();
 var Scraper = require('./scraper.js')
 app.get('/', function (req, res) {
 
+  casper.start('http://www.super-resume.com/resume-examples/', function() {
+    //this.echo(this.getTitle());
+    console.log('hi');
+  });
 
 
-    //var link ='http://www.super-resume.com'+ $('.resume').eq(12).children().attr('href');
+  //function that gets all of the links on a given page
+  var getAllLinks = function() {
+    //NOTE: can we scroll down before grabbing?
+    var linksOnThisPage = []
+    $("a[href^='/ResumeB']").each(function(index, linkDiv) {
+      $linkDiv = $(linkDiv)
+      linksOnThisPage.push('http://www.super-resume.com' + $linkDiv.attr('href'))
+    });
+    return linksOnThisPage
+  };
 
-  casper.start('http://www.super-resume.com/ResumeBuilder.jtp?query=Computer+Programmer', function() {
-      this.echo(this.getTitle());
-      console.log('hi');
+  var allLinks = [];
+  //build allLinks array by grabbing from desired pages
+  casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Computer+Programmer', function() {
+    allLinks = allLinks.concat(casper.evaluate(getAllLinks))
+  });
+  casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Computer+Support+Specialist', function() {
+    allLinks = allLinks.concat(casper.evaluate(getAllLinks))
+  });
+  casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Computer+Systems+Administrator', function() {
+    allLinks = allLinks.concat(casper.evaluate(getAllLinks))
+  });
+  casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Computer+Systems+Analyst', function() {
+    allLinks = allLinks.concat(casper.evaluate(getAllLinks))
+  });
+  casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Database+Administrator', function() {
+    allLinks = allLinks.concat(casper.evaluate(getAllLinks));
+    console.log(allLinks.length);
+
+    var resumesArray = []
+    allLinks.forEach(function (link, index) {
+      casper.thenOpen('link', function() {
+        var resume = casper.evaluate(Scraper.resumeScraper);
+        resumesArray.push(resume);
+        console.log('yo thats scrapped')
+
+        if (index == allLinks.length-1) {
+          console.log('you scrapped', resumesArray.length, 'resumes');
+          console.log(resumesArray);
+          res.send(resumesArray)
+        }
+      });
+    })
 
 
-  //can we scroll down before grabbing?
-    var links =  casper.evaluate(function () {
-       var linksArray=[]
-        $("a[href^='/ResumeB']").each(function(index, linkDiv){
-          $linkDiv= $(linkDiv)
-          linksArray.push('http://www.super-resume.com'+$linkDiv.attr('href'))
-
-        });
-    return linksArray
-     })
-    console.log(links);
   });
 
 
 
 
-  casper.thenOpen('links[0]', function() {
-
-      console.log( casper.evaluate(Scraper.resumeScraper).summary.Info )
-  });
 
   casper.run();
+
+
 
 
 }) // end of get
