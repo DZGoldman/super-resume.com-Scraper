356e1fc5af7b471b84231d392329931132b5b81b
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-02-29 15:46:25 -0500

    changes

--- casper2.js
+++ casper2.js
@@ -3,12 +3,14 @@ casper.start('http://google.com/', function() {
     // search for 'casperjs' from google form
     this.fill('form[action="/search"]', { q: 'a picture is worth a thousand divs' }, true);
 });
-
-
+// 
+// casper.then(function () {
+//  this.click('header p a:first-child');
+// })
 
 casper.then(function() {
 console.log('take a picture');
-casper.capture("screenshot.png")
+casper.capture("search.png")
 });
 
 casper.run();
