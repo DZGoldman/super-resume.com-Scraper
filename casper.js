6bb68a2a68ccfe836f769487431b6fee423d2ce8
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-03-20 10:48:22 -0400

    failing

--- casper.js
+++ casper.js
@@ -1,5 +1,6 @@
 var casper = require('casper').create();
-var Scraper = require('./scraper.js')
+var Scraper = require('./scraper.js');
+var fs = require('fs')
 
 //This generates a seed file in the console log window. No, like, literally.
 
@@ -21,6 +22,7 @@ var allLinks = [];
 
 //scroller
 var scrollDown = function() {
+   console.log('scrolling down');
   $('#search-left-inner').scrollTop(1000000);
 };
 
@@ -28,6 +30,7 @@ var loadAllLinksOnPage = function(category) {
   var prefix = 'http://www.super-resume.com/ResumeBuilder.jtp?query='
   casper.thenOpen(prefix+category)
     .thenEvaluate(scrollDown)
+
     .wait(2500, function() {}).thenEvaluate(scrollDown)
     // .wait(2500, function() {}).thenEvaluate(scrollDown)
     // .wait(2500, function() {}).thenEvaluate(scrollDown)
@@ -47,69 +50,34 @@ var loadAllLinksOnPage = function(category) {
     })
 }
 
-var categoriesArray =[ 'Computer+Support+Specialist','Computer+Systems+Administrator','Computer+Systems+Analyst','Web+Developer','Game+Developer','Mobile+Application+Developer','IT+Project+Manager','Information+Security+Analyst','Mechanical+Engineer','QA+Software+Tester','Software+Developer','Computer+Programmer','Database+Administrator']
+var categoriesArray =[ 'Computer+Support+Specialist','Computer+Systems+Administrator'
+//
+// ,'Computer+Systems+Analyst','Web+Developer','Game+Developer','Mobile+Application+Developer','IT+Project+Manager','Information+Security+Analyst','Mechanical+Engineer','QA+Software+Tester','Software+Developer','Computer+Programmer','Database+Administrator'
+]
 
 categoriesArray.forEach(function (category) {
+  console.log('loading a new category');
 loadAllLinksOnPage(category)
 })
 
 casper.thenOpen('http://www.super-resume.com/ResumeBuilder.jtp?query=Database+Administrator', function() {
 
   var resumesArray = []
-  console.log('var resumes = [');
+  console.log('i found ', allLinks.length, 'links');
 
   allLinks.forEach(function(link, index) {
     //enter each url
+
     casper.thenOpen(link, function() {
-      console.log('{');
-      //scrape the page
+      console.log('opened a new url');
       var resume = casper.evaluate(Scraper.resumeScraper);
-      console.log('title:', "'" + resume.title + "',");
-      console.log('summary:', "'" + resume.summary + "',");
 
-      console.log('experiences:', "[");
-      var experiences = resume.experiences;
-      if (experiences.length == 0) {
-        console.log(']');
-      }
-      experiences.forEach(function(exp, index) {
-        if (index == experiences.length - 1) {
-          console.log("'", exp, "']");
-        } else {
-          console.log("'", exp, "',");
-        }
-      })
-
-      if (index == allLinks.length - 1) {
-        console.log('}');
-        console.log('];');
-
-        console.log('');
-        console.log("var mongoose = require('mongoose'); var ResumeData = require('./models/resume_data.js');")
-        console.log("mongoose.connect('mongodb://localhost/resu-me', function(err) {")
-        console.log("if (err) {")
-        console.log("console.log('connection error', err);")
-        console.log("} else {")
-        console.log("console.log('connection successful bro');")
-        console.log("}")
-        console.log("});")
-        console.log('ResumeData.collection.insert(resumes, onInsert);')
-        console.log('function onInsert(err,docs) {')
-        console.log('if (err) {')
-        console.log("console.log('seed error, ruh roh...');")
-        console.log("}else {")
-        console.log("console.info('%d resumedatas were successfully stored.', docs.length);")
-        console.log("}")
-        console.log("}")
-        console.log('/*');
-
-
-      } else {
-        console.log('},');
-      }
       resumesArray.push(resume);
     });
   })
+
+  // save resumesArray
+  fs.write(resumestest, resumesArray, 'w')
 
 });
 
